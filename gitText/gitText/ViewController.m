3649dc948e6658d3587d5d3ae421645f3099ae1d
cc大川川 <799441940@qq.com>
2016-06-29 15:27:01 +0800

    初始化项目

---
+++ gitText/gitText/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  gitText
+//
+//  Created by wxhl on 16/6/29.
+//  Copyright © 2016年 chengchuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
